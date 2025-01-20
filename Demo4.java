235c5adeda3eeb3738c82c49b2c4f5f0f11203e9
siddhartha34248 <siddharthanayaki34@gmail.com>
2025-01-20 18:37:30 +0530

    Add files via upload

---
+++ Demo4.java
@@ -0,0 +1,9 @@
+package Core_java;
+
+public class Demo4 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
