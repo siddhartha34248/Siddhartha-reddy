156a3f83d35ee115e60098a12d5539ad26a386f1
siddhartha34248 <siddharthanayaki34@gmail.com>
2025-01-23 08:14:48 +0530

    Add files via upload

---
+++ Demo017.java
@@ -0,0 +1,16 @@
+class SumOftwoDigits {
+    public static int getSumOftwoDigits(int n) {
+        int a = n / 10;
+        int b = n % 10;
+        int sum = a + b;
+        return sum;
+    }
+}
+
+public class Demo017 {
+    public static void main(String[] args) {
+        int n = 78;
+        int sum = SumOftwoDigits.getSumOftwoDigits(n);
+        System.out.println("The sum of two digits is : " + sum);
+    }
+}
