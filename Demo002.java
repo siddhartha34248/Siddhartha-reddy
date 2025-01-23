009534d3927e07b7ef5a102aec908428e40909b0
siddhartha34248 <siddharthanayaki34@gmail.com>
2025-01-23 08:11:49 +0530

    Add files via upload

---
+++ Demo002.java
@@ -0,0 +1,11 @@
+public class Demo002{
+    public static void main(String args[]){
+        int x = 8;
+        int y = 7;
+        if(x>y){
+            System.out.println(x);
+        }
+        else
+        System.out.println(y);
+        }
+    }
