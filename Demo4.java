72b5a936317b566cb26f7cba6cb8e9b1f0d48afd
siddhartha34248 <siddharthanayaki34@gmail.com>
2025-01-23 08:21:07 +0530

    Delete Demo4.java

--- Demo4.java
+++
@@ -1,9 +0,0 @@
-package Core_java;
-
-public class Demo4 {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 100; i++) {
-            System.out.println(i);
-        }
-    }
-}
