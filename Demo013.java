6b3ef895cdb93e6c6c2641b6e44a45ce2baaf9c8
siddhartha34248 <siddharthanayaki34@gmail.com>
2025-01-23 08:13:43 +0530

    Add files via upload

---
+++ Demo013.java
@@ -0,0 +1,15 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+import java.util.Scanner;
+
+public class Demo13 {
+   public Demo13() {
+   }
+
+   public static void main(String[] var0) {
+      Scanner var1 = new Scanner(System.in);
+      System.out.println("Enter the radius of the circle: ");
+      double var2 = var1.nextDouble();
+      double var4 = var2 * var2 * 3.14;
+      System.out.println("Area of Circle: " + var4);
+   }
+}
