ec806ff639f8645c5000db77e2908c4bc71b6b92
siddhartha34248 <siddharthanayaki34@gmail.com>
2025-01-23 08:12:41 +0530

    Add files via upload

---
+++ Demo005.java
@@ -0,0 +1,11 @@
+public class Demo005 {
+    public static void main(String args[]) {
+        String username = "Bharath14";
+        String pas = "Bharath@14";
+        while (username == "Bharath14" & pas == "Bharath@14") {
+            System.out.println("Good Afternoon");
+            break;
+        }
+    }
+
+}
