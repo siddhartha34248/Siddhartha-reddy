edbfaf6eab100ba8cadb0cbfc8855f4bb393682a
siddhartha34248 <siddharthanayaki34@gmail.com>
2025-01-23 08:18:12 +0530

    Add files via upload

---
+++ Demo029.java
@@ -0,0 +1,9 @@
+public class Demo029 {
+    public static void main(String[] args){
+    String[] arr = {"Sathwik","Sai","Siddu","Abhi","Uday"};
+    for(int i=0;i<arr.length;i++){
+        System.out.println("Friends Name:"+arr[i]);
+    }
+}
+
+}
